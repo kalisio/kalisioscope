fb9052fc5d31776d91144c9ade85176befca0f02
Robin Bourianes <robin.bourianes@kalisio.com>
2024-06-10 14:59:34 +0200

    ci: updated github action ci

---
+++ generate-all.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -euo pipefail
+# set -x
+
+for i in kaabah kano kalisio kargo; do
+    ./generate.sh "$i" assets
+done
+
+for i in crisis field kapp kdk krawler maps planet skeleton teams watch weacast; do
+    ./generate2.sh "$i" assets
+done
+
+# Use rclone to copy on remote location
+sudo apt-get --no-install-recommends --yes install rclone
+
+export RCLONE_CONFIG_STORE_TYPE="s3"
+export RCLONE_CONFIG_STORE_PROVIDER="Other"
+export RCLONE_CONFIG_STORE_REGION="gra"
+export RCLONE_CONFIG_STORE_ENDPOINT="https://s3.gra.io.cloud.ovh.net/"
+# These are defined as secrets in the CI
+# export RCLONE_CONFIG_STORE_ACCESS_KEY_ID=""
+# export RCLONE_CONFIG_STORE_SECRET_ACCESS_KEY=""
+rclone copy assets store:kalisioscope
