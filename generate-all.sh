3e1a0bd8d17d68b3137f854fe91e5d9d59f97eed
Robin Bourianes <robin.bourianes@kalisio.com>
2024-06-10 15:20:38 +0200

    ci: fixed ci script

--- generate-all.sh
+++ generate-all.sh
@@ -16,8 +16,8 @@ sudo apt-get --no-install-recommends --yes install rclone
 export RCLONE_CONFIG_STORE_TYPE="s3"
 export RCLONE_CONFIG_STORE_PROVIDER="Other"
 export RCLONE_CONFIG_STORE_REGION="gra"
-export RCLONE_CONFIG_STORE_ENDPOINT="https://s3.gra.io.cloud.ovh.net/"
+export RCLONE_CONFIG_STORE_ENDPOINT="https://s3.gra.cloud.ovh.net/"
 # These are defined as secrets in the CI
 # export RCLONE_CONFIG_STORE_ACCESS_KEY_ID=""
 # export RCLONE_CONFIG_STORE_SECRET_ACCESS_KEY=""
-rclone copy assets store:kalisioscope
+rclone copy assets store:kalisioscope --stats 5s --stats-log-level NOTICE --stats-one-line
